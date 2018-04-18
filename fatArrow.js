ed3103ab6a8338f8112aa994c8a90eca317daf9c
RoshniPreman <roshnipreman92@gmail.com>
2018-04-18 17:26:02 +0530

    changed the fun

--- fatArrow.js
+++ fatArrow.js
@@ -3,3 +3,13 @@ console.log(add(1, 2));
 setTimeout(function () {
     console.log("called set timeout");
 }, 1000);
+var obj = {
+    name: 'Roshni',
+    printName: function () {
+        var _this = this;
+        setTimeout(function () {
+            console.log(_this.name);
+        }, 1000);
+    }
+};
+obj.printName();
--- fatArrow.ts
+++ fatArrow.ts
@@ -5,3 +5,15 @@ console.log(add(1 , 2));
 setTimeout(function(){
     console.log("called set timeout");
 } , 1000);
+
+let obj = {
+        name : 'Roshni',
+        printName : function(){
+
+            setTimeout(() => {
+                console.log(this.name);
+            }, 1000);           
+        }
+}
+
+obj.printName();
