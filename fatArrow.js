730a9439145b302956251ea77ae538fc2134dffe
RoshniPreman <roshnipreman92@gmail.com>
2018-04-18 17:34:02 +0530

    added comments

--- fatArrow.js
+++ fatArrow.js
@@ -1,3 +1,4 @@
+var _this = this;
 var add = function (a, b) { return a + b; };
 console.log(add(1, 2));
 setTimeout(function () {
@@ -6,9 +7,14 @@ setTimeout(function () {
 var obj = {
     name: 'Roshni',
     printName: function () {
-        var _this = this;
         setTimeout(function () {
             console.log(_this.name);
+            //  the value of this in a function depends on how the function is called.
+            // if we use fat arrow functions the value of this inside a fat arrow function will be the same as the value of this outside the fat arrow function.
+            //It uses the value of this from the surrounding code for its context. i.e. whatever this points to in the surrounding code, 
+            // this will point to in the function body of the fat arrow function.
+
+            // Arrow functions don't have their own this or arguments binding
         }, 1000);
     }
 };
--- fatArrow.ts
+++ fatArrow.ts
@@ -8,10 +8,17 @@ setTimeout(function(){
 
 let obj = {
         name : 'Roshni',
-        printName : function(){
+        printName : () => {
 
             setTimeout(() => {
                 console.log(this.name);
+
+                //  the value of this in a function depends on how the function is called.
+                // if we use fat arrow functions the value of this inside a fat arrow function will be the same as the value of this outside the fat arrow function.
+
+                //It uses the value of this from the surrounding code for its context. i.e. whatever this points to in the surrounding code, 
+                // this will point to in the function body of the fat arrow function.
+
             }, 1000);           
         }
 }
